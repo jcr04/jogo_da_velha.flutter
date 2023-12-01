3c0d4e59c0ed8dd6129a2fbac2a2fb3e8cf55b65
jcr04 <joaocarlosreboucas15@gmail.com>
2023-12-01 15:01:07 -0300

    add nes pages

--- tictactoe/lib/pages/start_page.dart
+++ tictactoe/lib/pages/start_page.dart
@@ -15,22 +15,54 @@ class StartPage extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GamePage()),
-                );
-              },
-              child: const Text('Start'),
+            // Logo ou Imagem
+            Image.asset('lib/assets/imagens/logo.png',
+                height: 100), // Substitua com o caminho do seu asset
+            const SizedBox(height: 30),
+
+            // Botão Start
+            _buildButton(
+              context,
+              'Start',
+              () => Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => GamePage())),
             ),
-            const SizedBox(height: 20), // Espaço entre os botões
-            ElevatedButton(
-              onPressed: () => _showCreditsDialog(context),
-              child: const Text('Créditos'),
-            ),
+
+            const SizedBox(height: 20),
+
+            // Botão Créditos
+            _buildButton(
+                context, 'Créditos', () => _showCreditsDialog(context)),
+
+            // Adicione mais botões ou widgets conforme necessário
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildButton(
+      BuildContext context, String label, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue, // Cor do texto do botão
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30), // Borda arredondada
+        ),
+        elevation: 5, // Sombra do botão
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min, // Para centralizar ícones e texto
+        children: [
+          Icon(label == 'Start'
+              ? Icons.play_arrow
+              : Icons.info_outline), // Ícone condicional
+          const SizedBox(width: 10), // Espaço entre ícone e texto
+          Text(label),
+        ],
       ),
     );
   }
@@ -40,13 +72,23 @@ class StartPage extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Créditos'),
+          title: const Text('Créditos',
+              style: TextStyle(fontWeight: FontWeight.bold)),
           content: const SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text('Jogo da Velha'),
-                Text('Desenvolvido por: [Seu Nome]'),
-                // Adicione mais linhas de créditos conforme necessário
+                // Logo ou imagem (opcional)
+                // Image.asset('caminho_para_imagem', height: 100),
+
+                Text('Jogo da Velha',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text('Desenvolvido por:',
+                    style: TextStyle(fontStyle: FontStyle.italic)),
+                Text('Nataly Eva Rodrigues'),
+                Text('Yan Henri'),
+                // adicionar link do repositorio
               ],
             ),
           ),
